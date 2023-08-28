ce12604bc1e71ed322a4e49aedd7a2fc3373ae37
Valeriy Lvov <tepliy_shov@mail.ru>
2023-08-28 21:51:51 +0500

    add DialogFlow learning script.

---
+++ add_intent.py
@@ -0,0 +1,39 @@
+import json
+
+with open("intent.json", "r") as my_file:
+    intent_json = my_file.read()
+intent = json.loads(intent_json)
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+questions = intent["Устройство на работу"]
+answer = intent.get("answer")
+
+create_intent('devmanvoicebot-ysve', 'to_get_job', questions, answer)

b8d200c2d621527c135c1d7f4cd911843eec2ed2
Valeriy Lvov <tepliy_shov@mail.ru>
2023-08-30 18:05:09 +0500

    add DialogFlow to VK bot.

--- VK_bot.py
+++ VK_bot.py
@@ -1,3 +1,4 @@
+import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from google.cloud import dialogflow
@@ -5,7 +6,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 
-async def detect_intent_texts(project_id, session_id, text, language_code):
+def detect_intent_texts(project_id, session_id, text, language_code):
 
     session_client = dialogflow.SessionsClient()
 
@@ -29,16 +30,15 @@ async def detect_intent_texts(project_id, session_id, text, language_code):
     )
     print("Fulfillment text: {}\n".format(response.query_result.intent))
 
-    # Send the fulfillment text back to the user on Telegram
-    await bot.send_message(chat_id=session_id, text=response.query_result.fulfillment_text)
+    vk_api.messages.send(user_id=event.user_id, message=response.query_result.fulfillment_text, random_id=random.randint(1,1000))
 
-load_dotenv(find_dotenv())
-vk_session = vk.VkApi(token=os.environ.get('TOKEN_VK'))
-session_api = vk_session.get_api()
-longpool = VkLongPoll(vk_session)
 
-for event in longpool.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            detect_intent_texts('devmanvoicebot-ysve', str(message.from_user.id), message.text, 'ru')
+if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+    vk_session = vk.VkApi(token=os.environ.get('TOKEN_VK'))
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            detect_intent_texts('devmanvoicebot-ysve', 'new', event.text, 'ru')
 

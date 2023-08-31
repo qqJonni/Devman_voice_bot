e45f0951a6581085cd324f3a41307c7bbaded100
Valeriy Lvov <tepliy_shov@mail.ru>
2023-08-31 18:50:41 +0500

    add README.md

--- bot.py
+++ bot.py
@@ -14,37 +14,43 @@ dp = Dispatcher(bot)
 @dp.message_handler()
 async def start_command(message: types.Message):
     # Call the detect_intent_texts function and pass the necessary parameters
-    await detect_intent_texts('devmanvoicebot-ysve', str(message.from_user.id), message.text, 'ru')
+    try:
+        await detect_intent_texts('devmanvoicebot-ysve', str(message.from_user.id), message.text, 'ru')
+    except Exception as e:
+        await bot.send_message(chat_id=message.chat.id, text=f"Произошла ошибка : {str(e)}")
 
 
 # Define the detect_intent_texts function
 async def detect_intent_texts(project_id, session_id, text, language_code):
     """Returns the intent detection result with text input.
     Using the same `session_id` between requests allows continuation of the conversation."""
-    session_client = dialogflow.SessionsClient()
+    try:
+        session_client = dialogflow.SessionsClient()
 
-    session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
+        session = session_client.session_path(project_id, session_id)
+        print("Session path: {}\n".format(session))
 
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+        query_input = dialogflow.QueryInput(text=text_input)
 
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
         )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.intent))
 
-    # Send the fulfillment text back to the user on Telegram
-    await bot.send_message(chat_id=session_id, text=response.query_result.fulfillment_text)
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.intent))
+
+        # Send the fulfillment text back to the user on Telegram
+        await bot.send_message(chat_id=session_id, text=response.query_result.fulfillment_text)
+    except Exception as e:
+        await bot.send_message(chat_id=session_id, text=f"Произошла ошибка: {str(e)}")
 
 
 # Start the bot
